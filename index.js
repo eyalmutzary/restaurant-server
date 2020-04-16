e528ef9eeb31c4a3a27c6a2a9938fe227b08347e
eyalmutzary <eyalmu1@gmail.com>
2020-04-16 20:53:45 +0300

    fixes

--- index.js
+++ index.js
@@ -11,30 +11,39 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(router);
 
-models.sequelize
-  .sync({ force: true })
-  .then((result) => {
-    app.listen(process.env.POSTGRES_PORT);
-    console.log(`Server Port: ${process.env.POSTGRES_PORT}`);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-// switch (process.env.NODE_ENV) {
-//   case "production":
-//     app.listen(process.env.POSTGRES_PORT);
-//     break;
-//   case "development":
+// models.sequelize
+//   .sync({ force: true })
+//   .then((result) => {
 //     app.listen(process.env.POSTGRES_PORT);
 //     console.log(`Server Port: ${process.env.POSTGRES_PORT}`);
-//     break;
-//   case "test":
-//     break;
-//   default:
-//     console.log("Unrecognized node environment");
-//     break;
-// }
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+switch (process.env.NODE_ENV) {
+  case "production":
+    app.listen(process.env.POSTGRES_PORT);
+    break;
+  case "development":
+    app.listen(process.env.POSTGRES_PORT);
+    models.sequelize
+      .sync({ force: true })
+      .then((result) => {
+        console.log("server synced");
+        console.log(`Server Port: ${process.env.POSTGRES_PORT}`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    console.log(`Server Port: ${process.env.POSTGRES_PORT}`);
+    break;
+  case "test":
+    break;
+  default:
+    console.log("Unrecognized node environment");
+    break;
+}
 
 app.use((err, req, res) => {
   console.log("Unexpected Error", err.stack);
